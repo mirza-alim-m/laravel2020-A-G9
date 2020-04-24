da6dfd54663660179759f6af625a963b330451e8
RivaldoWilly <aldowr7@gmail.com>
2020-04-24 14:36:04 +0700

    tugas

--- resources/views/buku/index.blade.php
+++ resources/views/buku/index.blade.php
@@ -65,7 +65,7 @@
         <div class="modal-content">
             <!-- heading modal -->
             <div class="modal-header">
-                <h4 class="modal-title">Data Peminjaman</h4>
+                <h4 class="modal-title">Data Buku</h4>
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
             </div>
             <!-- body modal -->
--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -63,7 +63,7 @@
         <div class="modal-content">
             <!-- heading modal -->
             <div class="modal-header">
-                <h4 class="modal-title">Data Anggota</h4>
+                <h4 class="modal-title">Data Category</h4>
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
             </div>
             <!-- body modal -->
