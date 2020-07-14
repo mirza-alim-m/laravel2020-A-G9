9711b34a0a328aff05a8034982a27252622c1189
AndikaPanjiPerkasa <andikapanji12@gmail.com>
2020-07-15 02:03:11 +0700

    Update

---
+++ resources/views/member/edit.blade.php
@@ -0,0 +1,81 @@
+@extends('admin.admin')
+
+@section('content')
+<div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Keanggotaan</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="{{ route('home') }}"><i class="fas fa-tachometer-alt"></i> Dashboard</a></li>
+              <li class="breadcrumb-item"><a href="{{ route('member.index') }}"><i class="fas fa-users"></i> Keanggotaan</a></li>
+              <li class="breadcrumb-item active"><i class="fa fa-edit"></i> Edit</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+<div class="card uper">
+    <div class="card-header">
+        Form Edit Data
+    </div>
+    <div class="card-body">
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div><br />
+        @endif
+
+        <form action="{{ route('member.update',$member->id) }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+
+            <div class="form-group">
+                <label>NIM</label>
+                <input type="text" class="form-control" name="nim" value="{{ $member->nim }}" readonly/>
+            </div>
+            <div class="form-group">
+                <label>Nama</label>
+                <input type="text" class="form-control" name="nama" value="{{ $member->nama }}" />
+            </div>
+            <div class="form-group">
+            <label for="inputjk">Jenis Kelamin</label>
+            <select id="inputjk" class="form-control" name="jk" required>
+                <option value="{{ $member->jk}}">{{ $member->jk}}</option>
+                <option value="Laki-Laki">Laki-Laki</option>
+                <option value="Perempuan">Perempuan</option>
+            </select>
+            </div>
+            <div class="form-group">
+                <label for="inputprodi">Prodi</label>
+                <select id="inputprodi" class="form-control" name="prodi" required>
+                    <option value="{{ $member->prodi}}">{{ $member->prodi}}</option>
+                    <option value="D3 Kebidanan">D3 Kebidanan</option>
+                    <option value="D3 Farmasi">D3 Farmasi</option>
+                    <option value="D3 Akuntansi">D3 Akuntansi</option>
+                    <option value="D3 Teknik Komputer">D3 Teknik Komputer</option>
+                    <option value="D3 Teknik Elektronika">D3 Teknik Elektronika</option>
+                    <option value="D3 Teknik Mesin">D3 Teknik Mesin</option>
+                    <option value="D3 Perhotelan">D3 Perhotelan</option>
+                    <option value="D3 Desain Komunikasi Visual">D3 Desain Komunikasi Visual</option>
+                    <option value="D4 Teknik Informatika">D4 Teknik Informatika</option>
+                    <option value="D4 Akuntansi Sektor Publik">D4 Akuntansi Sektor Publik</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label>Ubah Foto Profil</label>
+                <input type="file" class="form-control-file" name="foto"/>
+            </div>
+            <div class="form-group">
+                <label>Ubah Berkas Document</label>
+                <input type="file" class="form-control-file" name="pdf"/>
+            </div>
+            <button type="submit" class="btn btn-primary">Simpan</button>
+    </div>
+
+    </form>
+</div>
+</div>
+@endsection
