6c8d84e14dd710f0e087580454e821ac38f311e1
AndikaPanjiPerkasa <andikapanji12@gmail.com>
2020-07-15 01:59:55 +0700

    Update

--- database/seeds/BukuSeeder.php
+++ database/seeds/BukuSeeder.php
@@ -14,16 +14,22 @@ class BukuSeeder extends Seeder
     public function run()
     {
         $faker = Faker::create('id_ID');
-        for($i = 1; $i <= 100; $i++){
+        for ($i = 1; $i <= 100; $i++) {
 
             DB::table('buku')->insert([
-                'category_id' => $faker->numberBetween(1,9),
+                'category_id' => $faker->numberBetween(1, 9),
                 'judul' => $faker->sentence($nbWords = 3, $variableNbWords = true),
                 'penerbit' => $faker->sentence($nbWords = 3, $variableNbWords = true),
                 'penulis' => $faker->name,
+<<<<<<< HEAD
                 'jumlah' => $faker->numberBetween(1,10),
                 'foto'=> '/buku/foto.png',
                 'pdf'=> '/document/buku/document.pdf',
+=======
+                'jumlah' => $faker->numberBetween(1, 10),
+                'foto' => 'foto',
+                'pdf' => 'pdf',
+>>>>>>> 1c9364c050d1e0ffaae44dd30c34cb304603c07c
                 'created_at' => now(),
                 'updated_at' => now()
             ]);
--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -1,33 +1,34 @@
 @extends('admin.admin')
-
+<!-- form input data Category -->
 @section('content')
 <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Buku</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="{{ route('home') }}"><i class="fas fa-tachometer-alt"></i> Dashboard</a></li>
-              <li class="breadcrumb-item"><a href="{{ route('category.index') }}"><i class="fab fa-buffer"></i> Category</a></li>
-              <li class="breadcrumb-item active"><i class="fa fa-edit"></i> Create</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
+  <div class="col-sm-6">
+    <h1 class="m-0 text-dark">Buku</h1>
+  </div><!-- /.col -->
+  <div class="col-sm-6">
+    <ol class="breadcrumb float-sm-right">
+      <li class="breadcrumb-item"><a href="{{ route('home') }}"><i class="fas fa-tachometer-alt"></i> Dashboard</a></li>
+      <li class="breadcrumb-item"><a href="{{ route('category.index') }}"><i class="fab fa-buffer"></i> Category</a></li>
+      <li class="breadcrumb-item active"><i class="fa fa-edit"></i> Create</li>
+    </ol>
+  </div><!-- /.col -->
+</div><!-- /.row -->
 <div class="card uper">
-    <div class="card-header">
-        Form Tambah Data
-    </div>
-    <div class="card-body">
-        @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div><br />
-        @endif
+  <div class="card-header">
+    Form Tambah Data
+  </div>
+  <div class="card-body">
+    @if ($errors->any())
+    <div class="alert alert-danger">
+      <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+      </ul>
+    </div><br />
+    @endif
 
+<<<<<<< HEAD
         <form method="post" action="{{ route('category.store') }}">
             @csrf
             <div class="form-group">
@@ -50,5 +51,16 @@
             <button type="submit" class="btn btn-primary">Tambah Data</button>
         </form>
     </div>
+=======
+    <form method="post" action="{{ route('category.store') }}">
+      @csrf
+      <div class="form-group">
+        <label>Nama Kategori</label>
+        <input type="text" class="form-control" name="name" autocomplete="off" required />
+      </div>
+      <button type="submit" class="btn btn-primary">Tambah Data</button>
+    </form>
+  </div>
+>>>>>>> 1c9364c050d1e0ffaae44dd30c34cb304603c07c
 </div>
 @endsection
